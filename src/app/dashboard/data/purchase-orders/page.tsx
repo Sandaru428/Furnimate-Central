f00d5e837cb856f91ab13d8251449cd70e2e57ff
Albert Benigius Siraiva <absiraivaws@gmail.com>
2025-09-17 12:32:37 +0000

    ec89190 verify the code change that mention and develop

--- src/app/dashboard/data/purchase-orders/page.tsx
+++ src/app/dashboard/data/purchase-orders/page.tsx
@@ -530,7 +530,7 @@ export default function PurchaseOrdersPage() {
                             render={({ field }) => (
                                 <FormItem>
                                 <FormLabel>Payment Method</FormLabel>
-                                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                <Select onValueChange={field.onChange} value={field.value}>
                                     <FormControl>
                                         <SelectTrigger>
                                             <SelectValue placeholder="Select a payment method" />
