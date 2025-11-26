9feed9badf03f93c6ab9a1e2079bf00d0880805f
Sandaru428 <chamarasudusinghe428@gmail.com>
2025-11-27 01:34:19 +0530

    .env.local file added

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth, type Auth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  "projectId": "furnimate-central",
-  "appId": "1:355246282722:web:4ea2798d7e3c1d324e4bbb",
-  "storageBucket": "furnimate-central.firebasestorage.app",
-  "apiKey": "AIzaSyD6jPBR7SuXvV7Jv-bnpvZFoFk9sUNFS4Q",
-  "authDomain": "furnimate-central.firebaseapp.com",
-  "measurementId": "G-WJ00H55Y0B",
-  "messagingSenderId": "355246282722"
+  "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "storageBucket": process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  "authDomain": process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  "measurementId": process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  "messagingSenderId": process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
 };
 
 // Initialize Firebase
